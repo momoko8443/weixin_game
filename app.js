242adb9e644a658e658d3c37f49c4ebe84c5c053
momoko8443 <momoko8443@163.com>
2015-07-21 17:58:17 +0800

    up

--- app.js
+++ app.js
@@ -25,85 +25,6 @@ app.use(bodyParser.urlencoded({
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.query());
 app.use(bodyParser.urlencoded({ extended: true }));
-/*
-var appID = "wx86280d314fb1fd70";
-var secret = "bb185ab1918499b43069517667abbac7";
-var redirect_url = "http://momoko8443.tunnel.mobi/";
-var getCodeUrl = "https://open.weixin.qq.com/connect/oauth2/authorize?appid="+appID+"&redirect_uri="+redirect_url+"&response_type=code&scope=snsapi_userinfo&state=1#wechat_redirect";
-
-var access_token;
-
-
-app.use('/connect',wechat('ukulele',function(req,res,next){
-	var message = req.weixin;
-	var fromUser = message.FromUserName;
-	var content = message.Content;
-	res.reply([
-	{
-		title: 'Click me',
-		description: getCodeUrl,
-		url: getCodeUrl
-	}]);
-}));
-var userlist = {};
-app.get('/', function (req, res) {
-	var code = req.query.code;
-	var getAccessTokenUrl = "https://api.weixin.qq.com/sns/oauth2/access_token?appid="+appID+"&secret="+secret+"&code="+code+"&grant_type=authorization_code";
-    
-	https.get(getAccessTokenUrl, function(res2) {
-        var json = "";
-		res2.on('data', function(d) {
-            json += d;
-		});
-        res2.on('end',function(){
-            var obj = JSON.parse(json);
-            access_token = obj.access_token;
-            var openid = obj.openid;
-            res.redirect("pages/test.html?openid="+openid);    
-        });
-	}).on('error', function(e) {
-	  console.error(e);
-	});
-});
-
-app.get('/connect', function (req, res) {	
-	var signature = req.query.signature;
-	var timestamp = req.query.timestamp;
-	var nonce = req.query.nonce;
-	var echostr = req.query.echostr;
-	var token = "ukulele";
-	
-	var arr = [timestamp,nonce,token];	
-	var tempString = arr.sort().join("");
-	tempString = sha1(tempString);
-	if(signature === tempString){
-		console.log('auth access');
-		res.write(echostr);
-	}else{
-		console.log('auth failed');
-		res.write(false);
-	}
-});
-
-app.get('/user/:openid',function(req,res){   
-    var openid = req.params.openid;
-    console.log("get user: "+openid);
-    var getUserInfoUrl = "https://api.weixin.qq.com/sns/userinfo?access_token="+access_token+"&openid="+openid;
-    https.get(getUserInfoUrl,function(res2){
-        var json = "";
-            res2.on('data', function(d) {
-                json += d;
-            });
-            res2.on('end',function(){
-                console.log(json);
-                var userInfo = JSON.parse(json);
-                res.send(userInfo);
-            });
-    }).on("error",function(e){
-        console.error(e);
-    });
-    
-});*/
 
 //get testings's list
 app.get('/testing',function(req,res){
@@ -221,3 +142,83 @@ app.delete('/testing/:id/question/:qid',function(req,res){
 var server = app.listen(port,function () {
     console.log('Listening on port %d', server.address().port);
 });
+
+/*
+var appID = "wx86280d314fb1fd70";
+var secret = "bb185ab1918499b43069517667abbac7";
+var redirect_url = "http://momoko8443.tunnel.mobi/";
+var getCodeUrl = "https://open.weixin.qq.com/connect/oauth2/authorize?appid="+appID+"&redirect_uri="+redirect_url+"&response_type=code&scope=snsapi_userinfo&state=1#wechat_redirect";
+
+var access_token;
+
+
+app.use('/connect',wechat('ukulele',function(req,res,next){
+	var message = req.weixin;
+	var fromUser = message.FromUserName;
+	var content = message.Content;
+	res.reply([
+	{
+		title: 'Click me',
+		description: getCodeUrl,
+		url: getCodeUrl
+	}]);
+}));
+var userlist = {};
+app.get('/', function (req, res) {
+	var code = req.query.code;
+	var getAccessTokenUrl = "https://api.weixin.qq.com/sns/oauth2/access_token?appid="+appID+"&secret="+secret+"&code="+code+"&grant_type=authorization_code";
+    
+	https.get(getAccessTokenUrl, function(res2) {
+        var json = "";
+		res2.on('data', function(d) {
+            json += d;
+		});
+        res2.on('end',function(){
+            var obj = JSON.parse(json);
+            access_token = obj.access_token;
+            var openid = obj.openid;
+            res.redirect("pages/test.html?openid="+openid);    
+        });
+	}).on('error', function(e) {
+	  console.error(e);
+	});
+});
+
+app.get('/connect', function (req, res) {	
+	var signature = req.query.signature;
+	var timestamp = req.query.timestamp;
+	var nonce = req.query.nonce;
+	var echostr = req.query.echostr;
+	var token = "ukulele";
+	
+	var arr = [timestamp,nonce,token];	
+	var tempString = arr.sort().join("");
+	tempString = sha1(tempString);
+	if(signature === tempString){
+		console.log('auth access');
+		res.write(echostr);
+	}else{
+		console.log('auth failed');
+		res.write(false);
+	}
+});
+
+app.get('/user/:openid',function(req,res){   
+    var openid = req.params.openid;
+    console.log("get user: "+openid);
+    var getUserInfoUrl = "https://api.weixin.qq.com/sns/userinfo?access_token="+access_token+"&openid="+openid;
+    https.get(getUserInfoUrl,function(res2){
+        var json = "";
+            res2.on('data', function(d) {
+                json += d;
+            });
+            res2.on('end',function(){
+                console.log(json);
+                var userInfo = JSON.parse(json);
+                res.send(userInfo);
+            });
+    }).on("error",function(e){
+        console.error(e);
+    });
+    
+});*/
